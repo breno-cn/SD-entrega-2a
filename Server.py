8f2a71f9760bd8283cc3e3389cd9c155ef719158
Breno <breno.cn@hotmail.com>
2022-02-16 21:12:57 -0300

    WIP: primeiros passos para calcular finger table

--- Server.py
+++ Server.py
@@ -1,8 +1,24 @@
+import sys
+import os
+import signal
 import grpc
 
 from Server_pb2_grpc import ServerServicer, ServerStub, add_ServerServicer_to_server
 from Server_pb2 import Request, Response
 from Hashtable import Hashtable
+
+from concurrent import futures
+
+print(os.getpid())
+
+def calculateFingerTable(_signum, _frame):
+    global serverServicer
+
+    print(f'Calculating finger table of server {serverServicer.n}...')
+
+    serverServicer.calculateFingerTable()
+
+signal.signal(signal.SIGUSR1, calculateFingerTable)
 
 def getHash(key: str):
     keyBytes = bytes(key, 'ascii')
@@ -25,7 +41,6 @@ def ping(address: str) -> bool:
         stub.ping(Request())
         response = True
     except Exception as e:
-        print(e)
         response = False
     finally:
         channel.close()
@@ -33,32 +48,50 @@ def ping(address: str) -> bool:
 
 class Server(ServerServicer):
     
-    def __init__(self, id: int, host: str, maxServers: int, maxId: int) -> None:
+    def __init__(self, n: int, host: str, m: int) -> None:
         super().__init__()
-        self.id = id
+        self.n = n
         self.host = host
         self.hashtable = Hashtable()
-        self.maxServers = maxServers
-        self.maxId = maxId
+        self.m = m                      # ammount of bits used by the hash function, also determinate the max network size
+        self.fingerTable = [0] * self.m
+        self.maxNodes = (2 ** self.m) + 1
         # self.fingerTable = [self.succ(self.id + (2 ** (i - 1))) for i in range(self.maxServers)]
 
         # print(self.fingerTable)
-        print(self.succ(self.id))
+        # print(self.succ(self.id))
 
 
-    def shouldIProcess(self, key: str) -> bool:
-        result = getHash(key) % self.maxId
-        
-        return (result - self.maxServers) < result < (result + self.maxServers)
+    # def shouldIProcess(self, key: str) -> bool:
+    #    result = getHash(key) % self.maxId
+    #    
+    #    return (result - self.maxServers) < result < (result + self.maxServers)
 
     def succ(self, addr: int):
         currentAddr = addr + 1
         while True:
             if ping(f'localhost:{currentAddr}'):
                 return currentAddr
-            currentAddr += 1
+            currentAddr = (currentAddr + 1) % self.maxNodes
 
     def ping(self, request, context):
         return Response()
 
-server = Server(300, 'localhost', 5, 455)
+    def calculateFingerTable(self):
+        for i in range(self.m):
+            self.fingerTable[i] = self.succ(((self.n + (2 ** (i))) % self.maxNodes))
+
+        print(f'finger table: {self.fingerTable}')
+
+# server = Server(300, 'localhost', 5, 455)
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+serverServicer = Server(n, 'localhost', m)
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+add_ServerServicer_to_server(serverServicer, server)
+
+server.add_insecure_port(f'[::]:{n}')
+server.start()
+
+server.wait_for_termination()
